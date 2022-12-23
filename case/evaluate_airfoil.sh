6f7c61189c1dcf6aec1ee21089b78c235015e45f
Ole Petersen <peteole2707@gmail.com>
2022-12-23 12:05:40 +0100

    Fixed running script

--- case/evaluate_airfoil.sh
+++ case/evaluate_airfoil.sh
@@ -4,10 +4,15 @@ json=$1
 p=$(echo $json | jq -r '.p')
 m=$(echo $json | jq -r '.m')
 t=$(echo $json | jq -r '.t')
+angle=$(echo $json | jq -r '.a')
+mkdir -p /tmp/airfoil/"$p"_"$m"_"$t"_"$angle"
+cp -r ./* /tmp/airfoil/"$p"_"$m"_"$t"_"$angle"
+cd /tmp/airfoil/"$p"_"$m"_"$t"_"$angle"
+pwd
 
 source /opt/OpenFOAM/OpenFOAM-10/etc/bashrc
 p=$p m=$m t=$t julia mesh_generation.jl
-gmsh -3 aerofoil.geo -format msh2
+gmsh -3 aerofoil.geo -format msh2 -nopopup
 gmshToFoam aerofoil.msh
 function setBoundaryType {
     foamDictionary constant/polyMesh/boundary -entry entry0/$1/type -set $2
@@ -15,3 +20,8 @@ function setBoundaryType {
 }
 setBoundaryType frontAndBackPlanes empty
 setBoundaryType AIRFOIL wall
+SILENT=1 ./run_at_angle.sh $angle
+
+C_D=$(tail -n 1 postProcessing/calcForceCoefficients/0/forceCoeffs.dat  | awk '{print "\t" $4}')
+C_L=$(tail -n 1 postProcessing/calcForceCoefficients/0/forceCoeffs.dat  | awk '{print "\t" $5}')
+echo "{\"C_L\": $C_L, \"C_D\": $C_D}"
